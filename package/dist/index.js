8e79b1a94901adf2b61f596db4ed8e32ebf9e16e
estrattonbailey <ericstrattonbailey@gmail.com>
2017-03-11 11:22:23 -0500

    Monorepo, update build

---
+++ package/dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.default = function () {
+  var o = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  var listeners = {};
+
+  var on = function on(e) {
+    var cb = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+    if (!cb) return;
+    listeners[e] = listeners[e] || { queue: [] };
+    listeners[e].queue.push(cb);
+  };
+
+  var emit = function emit(e) {
+    var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+    var items = listeners[e] ? listeners[e].queue : false;
+    items && items.forEach(function (i) {
+      return i(data);
+    });
+  };
+
+  return _extends({}, o, {
+    emit: emit,
+    on: on
+  });
+};
